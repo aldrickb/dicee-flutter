dce70c6d795ee41e716e6dddee0fe41948e959e9
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2020-06-17 07:06:53 +0800

    Import dart math and use math random()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -22,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -33,7 +35,7 @@ class _DicePageState extends State<DicePage> {
               child: FlatButton(
                 onPressed: () {  
                   setState(() {
-                    leftDiceNumber = 2;
+                    leftDiceNumber = Random().nextInt(6) + 1;
                   });
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')
@@ -41,8 +43,12 @@ class _DicePageState extends State<DicePage> {
             ),
             Expanded(
               child: FlatButton(
-                onPressed: () {  },
-                child: Image.asset('images/dice1.png')
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')
               )
             )
           ],
